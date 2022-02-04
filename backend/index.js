d5c341825d20e7eb9bcd4ad1ccd6f3fb6125ec03
mattndross <mattndross@users.noreply.github.com>
2022-02-04 12:24:25 +0100

    connect to database and check for errors

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import app from "./server"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+dotenv.config();
+const MongoClient = mongodb.MongoClient;
+
+const port  = process.env.PORT || 8000;
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {poolSize:50, //max 50 connections at the same time
+    wtimeout:250, //cancel the connection if it takes more than 250seconds
+    useNewUrlParse: true,}
+)
+.catch(()=>{
+    console.error(err.stack);
+    process.exit(1)
+})
