ec4c6613b36a947bb97b89241b73cb197c71e979
mattndross <mattndross@users.noreply.github.com>
2022-02-07 19:20:31 +0100

    organize files moves dao directory inside backend directory

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
-import restaurantsDAO from "./api/dao/restaurantsDAO"
+import restaurantsDAO from "./dao/restaurantsDAO"
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
 
